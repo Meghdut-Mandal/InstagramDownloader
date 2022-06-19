fe38b742f427ddca8e2b06ee069d24a01553fe31
Meghdut Mandal <meghdut.mandal@autodesk.com>
2022-06-19 17:55:42 +0530

    Dashboard to Download rename

---
+++ app/src/main/java/com/meghdut/instagram/downloader/view/ui/dashboard/DownloadsViewModel.kt
@@ -0,0 +1,11 @@
+package com.meghdut.instagram.downloader.view.ui.dashboard
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DownloadsViewModel(application: Application) : AndroidViewModel(application) {
+
+}
